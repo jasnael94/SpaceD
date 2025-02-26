5c1d2afecf44f4ecb70e9a2e7856aefa5c453732
Jason <jasnael94@gmail.com>
2025-02-26 15:01:28 +0100

    first controller

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -1,16 +1,17 @@
 <?php
 
 namespace App\Controller;
-
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 class ProgramController extends AbstractController
 {
+    #[Route('/program/', name: 'program_index')]
     public function index(): Response
     {
         return new Response(
-            '<html><body>Donkey Series Index</body></html>'
+            '<!doctype html><html lang="en"><title>Donkey Series</title><body>Donkey Series Index</body></html>'
         );
     }
 }
